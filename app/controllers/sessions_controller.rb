1a87b57a3132f6b26456007fccda10228c5bbbd6
Doug Kintop <dkintop@hotmail.com>
2019-11-20 10:40:41 -0500

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,13 @@
 class SessionsController < ApplicationController
 
-    
+    def create 
+        User.create(user_params)
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :password, :password_confirmation)
+    end
 
 end
