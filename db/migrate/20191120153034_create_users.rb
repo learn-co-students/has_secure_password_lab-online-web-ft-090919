093ee40449176d691eae02d940fd45e12ea6a880
Doug Kintop <dkintop@hotmail.com>
2019-11-20 10:34:11 -0500

    Done.

--- db/migrate/20191120153034_create_users.rb
+++ db/migrate/20191120153034_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.0]
     create_table :users do |t|
       t.string, :password_digest
       t.string, :name
-      t.string :password_confirmation
+      
     end
   end
 end
