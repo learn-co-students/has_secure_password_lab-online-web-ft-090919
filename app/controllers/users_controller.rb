d8681a57a9f8ef269569c8fc7dd4727894c65a2b
Doug Kintop <dkintop@hotmail.com>
2019-11-20 11:02:59 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,19 @@
 class UsersController < ApplicationController
+    def new
 
+    end
+
+    def create
+        user = User.create(user_params)
+        if params[:password] == params[:passowrd_confirmation]
+        session[:user_id] = user.id
+        else 
+        redirect new_user_path
+        end
+    end
+
+    private 
+    def user_params
+        params.require(:user).permit(:name, :password, :password_confirmation)
+    end
 end
